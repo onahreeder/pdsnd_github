0db4deeb04b0c66b486f03e3e31626dd8ef85432
Onah Reeder <onah.reeder@volvo.com>
2022-03-15 09:50:17 -0400

    refactor: Add reference to day_name method
    Old (.2) Version of pandas required for project
    Old version uses weekday_name
    Later version of pandas uses day_name method
    Both lines of code provided so one can be commented out
    depending on panda version used

--- bikeshare.py
+++ bikeshare.py
@@ -19,7 +19,7 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
 
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington). Use a while loop to handle invalid inputs
 
     citylist = ["chicago", "new york city", "washington"]
     monthlist = ["january", "february", "march", "april", "may", "june", "all"]
